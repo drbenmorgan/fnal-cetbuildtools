4da7454d94c09107e136143f7a057659852edeae
Lynn Garren <garren@fnal.gov>
2010-11-29 14:49:14 -0600

    remove duplicate macro

--- Modules/BuildPlugins.cmake
+++ Modules/BuildPlugins.cmake
@@ -22,35 +22,6 @@ set( GENREFLEX_INCLUDES -I ${CPPUNIT_INC}
 			-I $ENV{MESSAGEFACILITY_INC}
 			-I $ENV{LIBSIGCPP_DIR}/include/sigc++-2.0
 			-I $ENV{LIBSIGCPP_DIR}/lib/sigc++-2.0/include )
-macro (build_dictionary maindir subdir)
-  add_library(${maindir}${subdir}_dict SHARED ${maindir}${subdir}_dict.cpp )
-  add_library(${maindir}${subdir}_map SHARED ${maindir}${subdir}_map.cpp )
-  set(dictionary_liblist "${ARGN}")
-  if( dictionary_liblist )
-    target_link_libraries( ${maindir}${subdir}_dict ${dictionary_liblist} )
-    target_link_libraries( ${maindir}${subdir}_map ${dictionary_liblist} )
-  endif( dictionary_liblist )
-  add_custom_command(
-     OUTPUT ${maindir}${subdir}_dict.cpp
-     COMMAND ${GENREFLEX} ${CMAKE_SOURCE_DIR}/art/${maindir}/${subdir}/classes.h
-        	 -s ${CMAKE_SOURCE_DIR}/art/${maindir}/${subdir}/classes_def.xml
-		 -I ${CMAKE_SOURCE_DIR}
-		 -I ${CMAKE_SOURCE_DIR}/art/${maindir}/${subdir}
-		 ${GENREFLEX_INCLUDES}
-        	 -o ${maindir}${subdir}_dict.cpp
-		 ${GENREFLEX_FLAGS} || { rm -f ${maindir}${subdir}_dict.cpp\; /bin/false\; }
-     DEPENDS ${CMAKE_SOURCE_DIR}/art/${maindir}/${subdir}/classes.h
-             ${CMAKE_SOURCE_DIR}/art/${maindir}/${subdir}/classes_def.xml
-  )
-  add_custom_command(
-     OUTPUT ${maindir}${subdir}_map.cpp
-     DEPENDS ${maindir}${subdir}_dict.cpp
-     COMMAND mv classes_ids.cc ${maindir}${subdir}_map.cpp
-  )
-  install ( TARGETS ${maindir}${subdir}_dict DESTINATION ${flavorqual_dir}/lib )
-  install ( TARGETS ${maindir}${subdir}_map  DESTINATION ${flavorqual_dir}/lib )
-endmacro (build_dictionary maindir subdir)
-
 macro (build_art_dictionary maindir subdir)
   add_library(${maindir}${subdir}_dict SHARED ${maindir}${subdir}_dict.cpp )
   add_library(${maindir}${subdir}_map SHARED ${maindir}${subdir}_map.cpp )
@@ -78,7 +49,7 @@ macro (build_art_dictionary maindir subdir)
   )
   install ( TARGETS ${maindir}${subdir}_dict DESTINATION ${flavorqual_dir}/lib )
   install ( TARGETS ${maindir}${subdir}_map  DESTINATION ${flavorqual_dir}/lib )
-endmacro (build_dictionary maindir subdir)
+endmacro (build_art_dictionary maindir subdir)
 
 # simple plugin libraries
 macro (simple_plugin name)
