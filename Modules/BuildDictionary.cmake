59a0b2f783c6356b830ad5fc3a4a33813780f340
Lynn Garren <garren@fnal.gov>
2010-12-07 16:36:16 -0600

    WORKING as desired

--- Modules/BuildDictionary.cmake
+++ Modules/BuildDictionary.cmake
@@ -5,10 +5,9 @@
 set( GENREFLEX_FLAGS --deep
                      --fail_on_warnings
 		     --capabilities=classes_ids.cc
-                     -DCMS_DICT_IMPL
 		     -D_REENTRANT
 		     -DGNU_SOURCE
-		     -DGNU_GCC
+		     -DGNU_GCC 
 		     -DPROJECT_NAME="${PROJECT_NAME}"
 		     -DPROJECT_VERSION="${version}" )
 
@@ -20,41 +19,38 @@ macro (generate_dictionary maindir subdir)
   endforeach(inc)
   set(dictname "${maindir}${subdir}" )
   add_custom_command(
-     OUTPUT ${PROJECT_BINARY_DIR}/${dictname}_dict.cpp  
+     OUTPUT ${PROJECT_BINARY_DIR}/dict/${dictname}_dict.cpp  
+            ${PROJECT_BINARY_DIR}/dict/${dictname}_map.cpp 
      COMMAND ${GENREFLEX} ${CMAKE_CURRENT_SOURCE_DIR}/classes.h
         	 -s ${CMAKE_CURRENT_SOURCE_DIR}/classes_def.xml
 		 -I ${CMAKE_SOURCE_DIR}
 		 -I ${CMAKE_CURRENT_SOURCE_DIR}
 		 ${GENREFLEX_INCLUDES} ${GENREFLEX_FLAGS}
-        	 -o ${dictname}_dict.cpp || 
-		 { ${CMAKE_COMMAND} -E remove -f ${dictname}_dict.cpp\; 
-	           ${CMAKE_COMMAND} -E remove -f classes_ids.cc\; 
-		   /bin/false\; }
+        	 -o ${dictname}_dict.cpp 
+     COMMAND ${CMAKE_COMMAND} -E copy classes_ids.cc ${dictname}_map.cpp
+     COMMAND ${CMAKE_COMMAND} -E remove -f classes_ids.cc
      DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/classes.h
              ${CMAKE_CURRENT_SOURCE_DIR}/classes_def.xml
   )
-  add_custom_command(
-     OUTPUT ${PROJECT_BINARY_DIR}/${dictname}_map.cpp
-     COMMAND ${CMAKE_COMMAND} -E copy classes_ids.cc ${dictname}_map.cpp
-     COMMAND ${CMAKE_COMMAND} -E remove -f classes_ids.cc
-     DEPENDS ${PROJECT_BINARY_DIR}/${dictname}_dict.cpp  
-  )
-  SET_SOURCE_FILES_PROPERTIES(${PROJECT_BINARY_DIR}/${dictname}_map.cpp
-                              PROPERTIES GENERATED 1)
+  add_custom_target( ${dictname}_generated 
+                     DEPENDS ${PROJECT_BINARY_DIR}/dict/${dictname}_dict.cpp 
+                             ${PROJECT_BINARY_DIR}/dict/${dictname}_map.cpp )
 endmacro (generate_dictionary)
 
 # dictionaries are built in art with this
 macro (build_dictionary maindir subdir)
   set(dictionary_liblist "${ARGN}" ${REFLEX} )
   set(dictname "${maindir}${subdir}" )
-  generate_dictionary( ${maindir} ${subdir} )
-  add_library(${dictname}_dict SHARED ${PROJECT_BINARY_DIR}/${dictname}_dict.cpp )
-  add_library(${dictname}_map  SHARED ${PROJECT_BINARY_DIR}/${dictname}_map.cpp )
-  SET_SOURCE_FILES_PROPERTIES(${PROJECT_BINARY_DIR}/${dictname}_dict.cpp 
-                              ${PROJECT_BINARY_DIR}/${dictname}_map.cpp
+  add_library(${dictname}_dict SHARED ${PROJECT_BINARY_DIR}/dict/${dictname}_dict.cpp )
+  add_library(${dictname}_map  SHARED ${PROJECT_BINARY_DIR}/dict/${dictname}_map.cpp )
+  SET_SOURCE_FILES_PROPERTIES(${PROJECT_BINARY_DIR}/dict/${dictname}_dict.cpp 
+                              ${PROJECT_BINARY_DIR}/dict/${dictname}_map.cpp
                               PROPERTIES GENERATED 1)
   target_link_libraries( ${dictname}_dict ${dictionary_liblist} )
   target_link_libraries( ${dictname}_map  ${dictionary_liblist} )
+  add_dependencies( ${dictname}_dict ${dictname}_generated )
+  add_dependencies( ${dictname}_map  ${dictname}_generated )
   install ( TARGETS ${dictname}_dict DESTINATION ${flavorqual_dir}/lib )
   install ( TARGETS ${dictname}_map  DESTINATION ${flavorqual_dir}/lib )
 endmacro (build_dictionary)
+
