80e8c79cf9a561e429bd335b686ce9183fa57b51
Lynn Garren <garren@fnal.gov>
2010-12-10 12:34:46 -0600

    add PROJECT_NAME to pluging library name

--- Modules/BuildPlugins.cmake
+++ Modules/BuildPlugins.cmake
@@ -3,10 +3,12 @@
 
 # simple plugin libraries
 macro (simple_plugin name)
-  add_library(${name} SHARED ${name}.cc )
+  set(plugin_name "${PROJECT_NAME}_${name}")
+  #message(STATUS "SIMPLE_PLUGIN: generating ${plugin_name}")
+  add_library(${plugin_name} SHARED ${name}.cc )
   set(simple_plugin_liblist "${ARGN}")
   if( simple_plugin_liblist )
-    target_link_libraries( ${name} ${simple_plugin_liblist} )
+    target_link_libraries( ${plugin_name} ${simple_plugin_liblist} )
   endif( simple_plugin_liblist )
-  install( TARGETS ${name}  DESTINATION ${flavorqual_dir}/lib )
+  install( TARGETS ${plugin_name}  DESTINATION ${flavorqual_dir}/lib )
 endmacro (simple_plugin name)
