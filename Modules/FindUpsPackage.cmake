b14c0c087e3cdd334256edc6cfb874f64896f06c
Lynn Garren <garren@fnal.gov>
2010-12-14 17:07:38 -0600

    version v1_6_0 implement minimum version checking

--- Modules/FindUpsPackage.cmake
+++ Modules/FindUpsPackage.cmake
@@ -6,6 +6,33 @@
 
 # since variables are passed, this is implemented as a macro
 
+#internal macro
+macro( _check_version product version minimum )
+   # convert vx_y_z to x.y.z
+   # must also recognize vx_y
+   STRING( REGEX REPLACE "v(.*)_(.*)_(.*)" "\\1.\\2" MINVER "${minimum}" )
+   STRING(REGEX MATCH [_] has_underscore ${MINVER})
+     if( has_underscore )
+       STRING( REGEX REPLACE "v(.*)_(.*)" "\\1.\\2" MINVER "${minimum}" )
+     endif( has_underscore )
+   STRING( REGEX REPLACE "v(.*)_(.*)_(.*)" "\\1.\\2.\\3" THISVER "${version}" )
+   STRING(REGEX MATCH [_] has_underscore ${THISVER})
+     if( has_underscore )
+       STRING( REGEX REPLACE "v(.*)_(.*)" "\\1.\\2" THISVER "${version}" )
+     endif( has_underscore )
+   #message(STATUS "${product} minimum version is ${MINVER} from ${minimum} " )
+   #message(STATUS "${product}  version is ${THISVER} from ${version} " )
+   if(  ${THISVER} STRGREATER ${MINVER} )
+     message( STATUS "${product} ${THISVER} meets minimum required version ${MINVER}")
+   else()
+     if(  ${THISVER} EQUAL ${MINVER} )
+       message( STATUS "${product} ${THISVER} meets minimum required version ${MINVER}")
+     else()
+       message( FATAL_ERROR "${product} ${THISVER} is less than minimum required version ${MINVER}")
+     endif()
+   endif()
+endmacro( _check_version product version minimum )
+
 macro( find_ups_product PRODUCTNAME version )
 
 # get upper and lower case versions of the name
@@ -17,8 +44,21 @@ set( ${PRODUCTNAME_UC}_VERSION $ENV{${PRODUCTNAME_UC}_VERSION} )
 if ( NOT ${PRODUCTNAME_UC}_VERSION )
   message(FATAL_ERROR "${PRODUCTNAME_UC} has not been setup")
 endif ()
+
+_check_version( ${PRODUCTNAME} ${${PRODUCTNAME_UC}_VERSION} ${version} )
+
 SET ( ${PRODUCTNAME_UC}_STRING $ENV{SETUP_${PRODUCTNAME_UC}} )
-STRING( REGEX REPLACE ".*([-][q]+ )(.*)([-][-j])" "-q \\2" ${PRODUCTNAME_UC}_QUAL "${${PRODUCTNAME_UC}_STRING}" )
+STRING( REGEX MATCH "[-][q]" has_qual  "${${PRODUCTNAME_UC}_STRING}" )
+STRING( REGEX MATCH "[-][j]" has_j  "${${PRODUCTNAME_UC}_STRING}" )
+if( has_qual )
+  if( has_j )
+     STRING( REGEX REPLACE ".*([-][q]+ )(.*)([-][-j])" "-q \\2" ${PRODUCTNAME_UC}_QUAL "${${PRODUCTNAME_UC}_STRING}" )
+  else( )
+     STRING( REGEX REPLACE ".*([-][q]+ )(.*)" "-q \\2" ${PRODUCTNAME_UC}_QUAL "${${PRODUCTNAME_UC}_STRING}" )
+  endif( )
+else( )
+  message(STATUS "${PRODUCTNAME_UC} has no qualifier")
+endif( )
 message(STATUS "${PRODUCTNAME_UC} version and qualifier are ${${PRODUCTNAME_UC}_VERSION} ${${PRODUCTNAME_UC}_QUAL}" )
 
 # add include directory to include path if it exists
