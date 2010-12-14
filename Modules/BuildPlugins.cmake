8a739ac1297ca320bf35ec51d512acec96b56124
Lynn Garren <garren@fnal.gov>
2010-12-14 14:08:16 -0600

    check for underscores

--- Modules/BuildPlugins.cmake
+++ Modules/BuildPlugins.cmake
@@ -3,9 +3,14 @@
 
 # simple plugin libraries
 macro (simple_plugin name)
-  set(plugin_name "${PROJECT_NAME}_${name}")
+  set(plugin_name "${PROJECT_NAME}_${name}_plugin")
+  set(codename "${name}_plugin.cc")
+  STRING(REGEX MATCH [_] has_underscore ${name})
+  if( has_underscore )
+     message(SEND_ERROR  "found underscore in plugin name: ${name}" )
+  endif( has_underscore )
   #message(STATUS "SIMPLE_PLUGIN: generating ${plugin_name}")
-  add_library(${plugin_name} SHARED ${name}.cc )
+  add_library(${plugin_name} SHARED ${codename} )
   set(simple_plugin_liblist "${ARGN}")
   if( simple_plugin_liblist )
     target_link_libraries( ${plugin_name} ${simple_plugin_liblist} )
