aa04936f31935b1bf9fbac64e1f3a0f0f7ee355d
Lynn Garren <garren@fnal.gov>
2011-01-03 16:09:21 -0600

    first pass at having simple plugin use a defined type

--- Modules/BuildPlugins.cmake
+++ Modules/BuildPlugins.cmake
@@ -1,8 +1,25 @@
 # macros for building plugin libraries
 #
+# Plugin type must be service, source, or module
 
 # simple plugin libraries
-macro (simple_plugin name)
+macro (simple_plugin name type)
+  set(plugin_name "${PROJECT_NAME}_${name}_${type}")
+  set(codename "${name}_${type}.cc")
+  STRING(REGEX MATCH [_] has_underscore ${name})
+  if( has_underscore )
+     message(SEND_ERROR  "found underscore in plugin name: ${name}" )
+  endif( has_underscore )
+  #message(STATUS "SIMPLE_PLUGIN: generating ${plugin_name}")
+  add_library(${plugin_name} SHARED ${codename} )
+  set(simple_plugin_liblist "${ARGN}")
+  if( simple_plugin_liblist )
+    target_link_libraries( ${plugin_name} ${simple_plugin_liblist} )
+  endif( simple_plugin_liblist )
+  install( TARGETS ${plugin_name}  DESTINATION ${flavorqual_dir}/lib )
+endmacro (simple_plugin name)
+
+macro (old_simple_plugin name)
   set(plugin_name "${PROJECT_NAME}_${name}_plugin")
   set(codename "${name}_plugin.cc")
   STRING(REGEX MATCH [_] has_underscore ${name})
@@ -16,7 +33,7 @@ macro (simple_plugin name)
     target_link_libraries( ${plugin_name} ${simple_plugin_liblist} )
   endif( simple_plugin_liblist )
   install( TARGETS ${plugin_name}  DESTINATION ${flavorqual_dir}/lib )
-endmacro (simple_plugin name)
+endmacro (old_simple_plugin name)
 
 # simple service libraries
 macro (simple_service name)
