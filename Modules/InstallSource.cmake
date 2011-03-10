17662875aad4a60370974bbab3f518843f534b35
Lynn Garren <garren@fnal.gov>
2011-03-10 15:30:52 -0600

    add more file extensions

--- Modules/InstallSource.cmake
+++ Modules/InstallSource.cmake
@@ -1,6 +1,8 @@
 # use to install source code
 #
 # install_source()
+# install_source( SUBDIRS dir1 )
+# install_source( SUBDIRS dir1 dir2 )
 
 include(CetParseArgs)
 
@@ -9,13 +11,13 @@ macro( install_source   )
   
   STRING( REGEX REPLACE "^${CMAKE_SOURCE_DIR}(.*)" "\\1" CURRENT_SUBDIR "${CMAKE_CURRENT_SOURCE_DIR}" )
   set(source_install_dir ${product}/${version}/source/${product}${CURRENT_SUBDIR} )
-  FILE(GLOB src_files "*.cc" "*.h" "*.cpp" )
+  FILE(GLOB src_files *.cc *.h *.cpp *.icc *.xml )
   INSTALL( FILES ${src_files} 
            DESTINATION ${source_install_dir} )
   message( STATUS "source code will be installed in is ${source_install_dir}" )
   if( ISRC_SUBDIRS )
      foreach( sub ${ISRC_SUBDIRS} )
-	FILE(GLOB src_files "${sub}/*.cc" "${sub}/*.h" "${sub}/*.cpp" )
+	FILE(GLOB src_files ${sub}/*.cc ${sub}/*.h ${sub}/*.cpp ${sub}/*.icc ${sub}/*.xml )
 	INSTALL( FILES ${src_files} 
         	 DESTINATION ${source_install_dir}/${sub} )
      endforeach(sub)
