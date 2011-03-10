c0b4d5cdfe75b4c493a489291036f05e902b3f6f
Lynn Garren <garren@fnal.gov>
2011-03-10 14:34:26 -0600

    can specify subdirectories if they don't have their own CMakeLists.txt

--- Modules/InstallSource.cmake
+++ Modules/InstallSource.cmake
@@ -2,16 +2,24 @@
 #
 # install_source()
 
-macro( install_source )
+include(CetParseArgs)
 
-STRING( REGEX REPLACE "^${CMAKE_SOURCE_DIR}(.*)" "\\1" CURRENT_SUBDIR "${CMAKE_CURRENT_SOURCE_DIR}" )
-set(source_install_dir ${product}/${version}/source/${product}${CURRENT_SUBDIR} )
-FILE(GLOB cc_files "*.cc" )
-FILE(GLOB cpp_files "*.cpp" )
-FILE(GLOB h_files "*.h" )
-INSTALL( FILES ${cc_files} ${cpp_files} ${h_files}
-         DESTINATION ${source_install_dir} )
-message( STATUS "source code will be installed in is ${source_install_dir}" )
-
+macro( install_source   )
+  cet_parse_args( ISRC "SUBDIRS" "" ${ARGN})
+  
+  STRING( REGEX REPLACE "^${CMAKE_SOURCE_DIR}(.*)" "\\1" CURRENT_SUBDIR "${CMAKE_CURRENT_SOURCE_DIR}" )
+  set(source_install_dir ${product}/${version}/source/${product}${CURRENT_SUBDIR} )
+  FILE(GLOB src_files "*.cc" "*.h" "*.cpp" )
+  INSTALL( FILES ${src_files} 
+           DESTINATION ${source_install_dir} )
+  message( STATUS "source code will be installed in is ${source_install_dir}" )
+  if( ISRC_SUBDIRS )
+     foreach( sub ${ISRC_SUBDIRS} )
+	FILE(GLOB src_files "${sub}/*.cc" "${sub}/*.h" "${sub}/*.cpp" )
+	INSTALL( FILES ${src_files} 
+        	 DESTINATION ${source_install_dir}/${sub} )
+     endforeach(sub)
+     message( STATUS "also installing in subdirs: ${ISRC_SUBDIRS}")
+  endif( ISRC_SUBDIRS )
 
 endmacro( install_source )
