baca8582e1a63095d66eeaa3aa96a83a7f88ec66
Lynn Garren <garren@fnal.gov>
2011-03-11 15:56:44 -0600

    find code generated from *.in files

--- Modules/InstallSource.cmake
+++ Modules/InstallSource.cmake
@@ -1,6 +1,6 @@
 # Use this macro to install source code for debugging
 # The default implementation will pick up the following file extensions:
-#          .cc .cpp .C .cxx .h .icc .hh .H .xml
+#         .c .cc .cpp .C .cxx .h .icc .hh .H .xml
 # For other files, use the LIST option
 # Note that the LIST and SUBDIRS options are mutually exclusive
 # The LIST option is intended to allow you to pick up extra files not otherwise found.
@@ -16,28 +16,41 @@
 
 include(CetParseArgs)
 
+macro( _cet_install_generated_code )
+  FILE(GLOB config_source_files *.in  )
+  if( config_source_files )
+     foreach( input_file ${config_source_files} )
+	STRING( REGEX REPLACE "^(${CMAKE_CURRENT_SOURCE_DIR})(.*)[.][i][n]$"  "${CMAKE_CURRENT_BINARY_DIR}\\2" output_file "${input_file}")
+	INSTALL( FILES ${output_file} 
+        	 DESTINATION ${source_install_dir} )
+     endforeach(input_file)
+  endif( config_source_files )
+endmacro( _cet_install_generated_code )
+
 macro( _cet_install_without_list   )
+  message( STATUS "source code will be installed in ${source_install_dir}" )
   FILE(GLOB src_files 
-            *.cc *.h *.cpp *.icc *.xml *.C *.cxx *.hh *.H )
+            *.c *.cc *.h *.cpp *.icc *.xml *.C *.cxx *.hh *.H )
   INSTALL( FILES ${src_files} 
            DESTINATION ${source_install_dir} )
-  message( STATUS "source code will be installed in is ${source_install_dir}" )
+  # check for generated files
+  _cet_install_generated_code()
+  # now check subdirectories
   if( ISRC_SUBDIRS )
      foreach( sub ${ISRC_SUBDIRS} )
-	FILE(GLOB src_files 
-	         ${sub}/*.cc  ${sub}/*.cpp ${sub}/*.C ${sub}/*.cxx
+	FILE(GLOB subdir_src_files 
+	         ${sub}/*.cc ${sub}/*.c ${sub}/*.cpp ${sub}/*.C ${sub}/*.cxx
 		 ${sub}/*.h ${sub}/*.icc  ${sub}/*.hh ${sub}/*.H
 		 ${sub}/*.xml  )
-	INSTALL( FILES ${src_files} 
+	INSTALL( FILES ${subdir_src_files} 
         	 DESTINATION ${source_install_dir}/${sub} )
      endforeach(sub)
      message( STATUS "also installing in subdirs: ${ISRC_SUBDIRS}")
   endif( ISRC_SUBDIRS )
-
 endmacro( _cet_install_without_list )
 
 macro( _cet_install_from_list   )
-   message( STATUS "source code will be installed in is ${source_install_dir}" )
+   message( STATUS "source code will be installed in ${source_install_dir}" )
    INSTALL( FILES ${ISRC_LIST} 
             DESTINATION ${source_install_dir} )
 endmacro( _cet_install_from_list )
