83bb03eee870e7a59860f175357cec086d74a2cb
Lynn Garren <garren@fnal.gov>
2011-03-11 10:12:17 -0600

    add LIST option

--- Modules/InstallSource.cmake
+++ Modules/InstallSource.cmake
@@ -1,16 +1,22 @@
-# use to install source code
+# Use this macro to install source code for debugging
+# The default implementation will pick up the following file extensions:
+#          .cc .cpp .C .cxx .h .icc .hh .H .xml
+# For other files, use the LIST option
+# Note that the LIST and SUBDIRS options are mutually exclusive
+# The LIST option is intended to allow you to pick up extra files not otherwise found.
+# You cannot pick up files in subdirectories with LIST.
+#
+# Recommended use:
+# install_source( [SUBDIRS subdirectory_list ] )
+# install_source( LIST extra_file_list )
 #
 # install_source()
-# install_source( SUBDIRS dir1 )
-# install_source( SUBDIRS dir1 dir2 )
+# install_source( SUBDIRS subdirectory_list )
+# install_source( LIST extra_file_list )
 
 include(CetParseArgs)
 
-macro( install_source   )
-  cet_parse_args( ISRC "SUBDIRS" "" ${ARGN})
-  
-  STRING( REGEX REPLACE "^${CMAKE_SOURCE_DIR}(.*)" "\\1" CURRENT_SUBDIR "${CMAKE_CURRENT_SOURCE_DIR}" )
-  set(source_install_dir ${product}/${version}/source/${product}${CURRENT_SUBDIR} )
+macro( _cet_install_without_list   )
   FILE(GLOB src_files 
             *.cc *.h *.cpp *.icc *.xml *.C *.cxx *.hh *.H )
   INSTALL( FILES ${src_files} 
@@ -27,5 +33,28 @@ macro( install_source   )
      endforeach(sub)
      message( STATUS "also installing in subdirs: ${ISRC_SUBDIRS}")
   endif( ISRC_SUBDIRS )
+
+endmacro( _cet_install_without_list )
+
+macro( _cet_install_from_list   )
+   message( STATUS "file list: ${ISRC_LIST}" )
+   message( STATUS "source code will be installed in is ${source_install_dir}" )
+   INSTALL( FILES ${ISRC_LIST} 
+            DESTINATION ${source_install_dir} )
+endmacro( _cet_install_from_list )
+
+macro( install_source   )
+  cet_parse_args( ISRC "SUBDIRS;LIST" "" ${ARGN})
+  STRING( REGEX REPLACE "^${CMAKE_SOURCE_DIR}(.*)" "\\1" CURRENT_SUBDIR "${CMAKE_CURRENT_SOURCE_DIR}" )
+  set(source_install_dir ${product}/${version}/source/${product}${CURRENT_SUBDIR} )
+  if( ISRC_LIST )
+     if( ISRC_SUBDIRS )
+        message( FATAL_ERROR 
+	         "ERROR: call install_source with EITHER LIST or SUBDIRS but not both")
+     endif( ISRC_SUBDIRS )
+     _cet_install_from_list()
+  else()
+      _cet_install_without_list()
+  endif()
 
 endmacro( install_source )
