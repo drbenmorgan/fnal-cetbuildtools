ff6b2ac3a5d6afed84fc5ffb3e80ab3eee8e13e3
Lynn Garren <garren@fnal.gov>
2011-03-31 11:39:23 -0500

    use subdirectory instead of project name and add NOINSTALL option

--- Modules/BuildPlugins.cmake
+++ Modules/BuildPlugins.cmake
@@ -2,12 +2,23 @@
 #
 # The plugin type is expected to be service, source, or module, 
 # but we do not enforce this.
+#
+# USAGE:
+# simple_plugin( <name> <plugin type> 
+#                [library list] 
+#                [ALLOW_UNDERSCORES] 
+#                [NOINSTALL] )
+#        the base plugin name is derived from the current source code subdirectory
+#        specify NOINSTALL when building a plugin for the tests
 
 # simple plugin libraries
 include(CetParseArgs)
 macro (simple_plugin name type)
-  cet_parse_args(SP "" "ALLOW_UNDERSCORES" ${ARGN})
-  set(plugin_name "${PROJECT_NAME}_${name}_${type}")
+  cet_parse_args(SP "" "ALLOW_UNDERSCORES;NOINSTALL" ${ARGN})
+  # base name on current subdirectory
+  STRING( REGEX REPLACE "^${CMAKE_SOURCE_DIR}/(.*)" "\\1" CURRENT_SUBDIR "${CMAKE_CURRENT_SOURCE_DIR}" )
+  STRING( REGEX REPLACE "/" "_" plugname "${CURRENT_SUBDIR}" )
+  set(plugin_name "${plugname}_${name}_${type}")
   set(codename "${name}_${type}.cc")
   if( NOT SP_ALLOW_UNDERSCORES )
     string(REGEX MATCH [_] has_underscore ${name})
@@ -21,5 +32,7 @@ macro (simple_plugin name type)
   if( simple_plugin_liblist )
     target_link_libraries( ${plugin_name} ${simple_plugin_liblist} )
   endif( simple_plugin_liblist )
-  install( TARGETS ${plugin_name}  DESTINATION ${flavorqual_dir}/lib )
+  if( NOT SP_NOINSTALL )
+    install( TARGETS ${plugin_name}  DESTINATION ${flavorqual_dir}/lib )
+  endif()
 endmacro (simple_plugin name type)
