092e1db8170149b12a73ea2fe4be8f786a71b730
Lynn Garren <garren@fnal.gov>
2011-04-01 11:36:26 -0500

    look for package subdirectories when building an integrated suite

---
+++ Modules/BuildSubdirectories.cmake
@@ -0,0 +1,18 @@
+
+
+macro( build_subdirectories )
+  message(STATUS "BUILD_SUBSIRECTORIES: called with ${ARGC} arguments: ${ARGV}")
+  set(build_subdirectories_usage "USAGE: build_subdirectories( <package list> )")
+  set(subdir_list ${ARGN})
+  message(STATUS "BUILD_SUBSIRECTORIES: CMAKE_CURRENT_SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR} ")
+  FILE(GLOB subdir_cmake_files */CMakeLists.txt  )
+  message(STATUS "BUILD_SUBSIRECTORIES: found ${subdir_cmake_files} ")
+
+  foreach( cdir ${subdir_cmake_files} )
+      get_filename_component(dir2 ${cdir} PATH)
+      get_filename_component(dir ${dir2} NAME)
+      message(STATUS "BUILD_SUBSIRECTORIES: ${cdir} -> ${dir2} -> ${dir}" )
+      message(STATUS "BUILD_SUBSIRECTORIES: including ${dir} ")
+      subdirs( ${dir} )
+  endforeach(dir)
+endmacro( build_subdirectories )
