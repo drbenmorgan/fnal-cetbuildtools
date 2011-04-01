aa298cac6589081a2aa433b1dc0501218a9b2fc1
Lynn Garren <garren@fnal.gov>
2011-04-01 16:13:08 -0500

    remove extra messages

--- Modules/BuildSubdirectories.cmake
+++ Modules/BuildSubdirectories.cmake
@@ -1,12 +1,12 @@
 
 
 macro( build_subdirectories )
-  message(STATUS "BUILD_SUBSIRECTORIES: called with ${ARGC} arguments: ${ARGV}")
+  #message(STATUS "BUILD_SUBSIRECTORIES: called with ${ARGC} arguments: ${ARGV}")
   set(build_subdirectories_usage "USAGE: build_subdirectories( <package list> )")
   set(subdir_list ${ARGN})
-  message(STATUS "BUILD_SUBSIRECTORIES: CMAKE_CURRENT_SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR} ")
+  #message(STATUS "BUILD_SUBSIRECTORIES: CMAKE_CURRENT_SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR} ")
   FILE(GLOB subdir_cmake_files */CMakeLists.txt  )
-  message(STATUS "BUILD_SUBSIRECTORIES: found ${subdir_cmake_files} ")
+  #message(STATUS "BUILD_SUBSIRECTORIES: found ${subdir_cmake_files} ")
 
   foreach( cdir ${subdir_cmake_files} )
       get_filename_component(dir2 ${cdir} PATH)
@@ -14,5 +14,5 @@ macro( build_subdirectories )
       message(STATUS "BUILD_SUBSIRECTORIES: ${cdir} -> ${dir2} -> ${dir}" )
       message(STATUS "BUILD_SUBSIRECTORIES: including ${dir} ")
       subdirs( ${dir} )
-  endforeach(dir)
+  endforeach(cdir)
 endmacro( build_subdirectories )
