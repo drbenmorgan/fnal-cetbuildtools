781de977998971350a0ea06de7bd86cde34fc75d
Lynn Garren <garren@fnal.gov>
2012-02-21 16:42:37 -0600

    make and install an executable

---
+++ example/ToyCmake/ToyCmake/Hello/Hello.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <assert.h>
+#include "ToyCmake/Math/DoMath.h"
+
+int main()
+{
+  ToyCmake::DoMath mth;
+
+  double x,y;
+  x = 4.;
+  assert( mth.calculate(x) == 16. );
+
+  y = 5.;
+  mth.setval(y);
+  assert( mth.calculate(x) == 80. );
+
+  return 0;
+}
+
