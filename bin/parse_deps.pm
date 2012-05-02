53e624a0caf0de2b68867d7b88b591bc1b7246ba
Lynn Garren <garren@fnal.gov>
2012-05-02 14:44:48 -0500

    allow products to be optional

--- bin/parse_deps.pm
+++ bin/parse_deps.pm
@@ -1,14 +1,18 @@
 # parse product_deps and qualifier_deps
 
 # product_deps format:
+
 #   parent this_product this_version
 #   [incdir      product_dir	include]
 #   [libdir      fq_dir	lib]
 #   [bindir      fq_dir	bin]
 #   product		version
-#   dependent_product	dependent_product_version
-#   dependent_product	dependent_product_version
-#
+#   dependent_product	dependent_product_version [optional]
+#   dependent_product	dependent_product_version [optional]
+#   qualifier dependent_product       dependent_product notes
+#   this_qual dependent_product_qual  dependent_product_qual
+#   this_qual dependent_product_qual  dependent_product_qual
+
 # The indir, libdir, and bindir lines are optional
 # Use them only if your product does not conform to the defaults
 # Format: directory_type directory_path directory_name
@@ -17,6 +21,17 @@
 # The third field is not constrained
 #
 # if dependent_product_version is a dash, the "current" version will be specified
+# If a dependent product is optional, then add "optional" to the third field. 
+
+#
+# Use as many rows as you need for the qualifiers
+# Use a separate column for each dependent product that must be explicitly setup
+# Do not list products which will be setup by a dependent_product
+#
+# special qualifier options
+# -	not installed for this parent qualifier
+# -nq-	this dependent product has no qualifier
+# -b-	this dependent product is only used for the build - it will not be in the table
 
 sub parse_product_list {
   my @params = @_;
@@ -74,21 +89,6 @@ sub parse_product_list {
   close(PIN);
   return ($prod, $ver, $dq, %phash);
 }
-
-# qualifier_deps format:
-#   parent this_product this_version
-#   qualifier dependent_product       dependent_product notes
-#   this_qual dependent_product_qual  dependent_product_qual
-#   this_qual dependent_product_qual  dependent_product_qual
-#
-# Use as many rows as you need for the qualifiers
-# Use a separate column for each dependent product that must be explicitly setup
-# Do not list products which will be setup by a dependent_product
-#
-# special qualifier options
-# -	not installed for this parent qualifier
-# -nq-	this dependent product has no qualifier
-# -b-	this dependent product is only used for the build - it will not be in the table
 
 sub parse_qualifier_list {
   my @params = @_;
@@ -174,6 +174,65 @@ sub parse_qualifier_list {
   close(QIN);
   ##print "found $irow qualifier rows\n";
   return ($qlen, @qlist);
+}
+
+sub find_optional_products {
+  my @params = @_;
+  open(PIN, "< $params[0]") or die "Couldn't open $params[0]";
+  $get_phash="";
+  $get_quals="";
+  while ( $line=<PIN> ) {
+    chop $line;
+    if ( index($line,"#") == 0 ) {
+    } elsif ( $line !~ /\w+/ ) {
+    } else {
+      @words = split(/\s+/,$line);
+      if( $words[0] eq "parent" ) {
+	 $get_phash="";
+         $get_quals="";
+      } elsif( $words[0] eq "no_fq_dir" ) {
+	 $get_phash="";
+         $get_quals="";
+      } elsif( $words[0] eq "incdir" ) {
+	 $get_phash="";
+         $get_quals="";
+      } elsif( $words[0] eq "libdir" ) {
+	 $get_phash="";
+         $get_quals="";
+      } elsif( $words[0] eq "bindir" ) {
+	 $get_phash="";
+         $get_quals="";
+      } elsif( $words[0] eq "defaultqual" ) {
+	 $get_phash="";
+         $get_quals="";
+	 $dq=@words[1];
+      } elsif( $words[0] eq "only_for_build" ) {
+	 $get_phash="";
+         $get_quals="";
+      } elsif( $words[0] eq "product" ) {
+	 $get_phash="true";
+         $get_quals="";
+      } elsif( $words[0] eq "qualifier" ) {
+	 $get_phash="";
+         $get_quals="true";
+      } elsif( $get_phash ) {
+	if ( $#words == 2 ) {
+	   if(  $words[2] eq "optional" ) {
+              $opthash{ $words[0] } = $words[2];
+	   } else {
+             $opthash{ $words[0] } = "";
+	   }
+	} else {
+          $opthash{ $words[0] } = "";
+	}
+      } elsif( $get_quals ) {
+      } else {
+        print "ignoring $line\n";
+      }
+    }
+  }
+  close(PIN);
+  return (%opthash);
 }
 
 sub get_include_directory {
@@ -318,6 +377,41 @@ sub cetpkg_info_file {
   print CPG "CETPKG_TYPE     $params[3]\n";
   close(CPG);
   return($cetpkgfile);  
+}
+
+sub print_setup_noqual {
+  my @params = @_;
+  my $efl = $params[3];
+  if( $params[2] eq "optional" ) { 
+  print $efl "# setup of $params[0] is optional\n"; 
+  print $efl "unset have_prod\n"; 
+  print $efl "ups exist $params[0] $params[1]\n"; 
+  print $efl "test \"\$?\" = 0 && set_ have_prod=\"true\"\n"; 
+  print $efl "test \"\$have_prod\" = \"true\" || echo \"will not setup $params[0] $params[1]\"\n"; 
+  print $efl "test \"\$have_prod\" = \"true\" && setup -B $params[0] $params[1] \n";
+  print $efl "unset have_prod\n"; 
+  } else {
+  print $efl "setup -B $params[0] $params[1] \n";
+  }
+  return 0;
+}
+
+sub print_setup_qual {
+  my @params = @_;
+  my $efl = $params[4];
+  if( $params[3] eq "optional" ) { 
+  print $efl "# setup of $params[0] is optional\n"; 
+  print $efl "unset have_prod\n"; 
+  print $efl "ups exist $params[0] $params[1] -q $params[2]\n"; 
+  print $efl "test \"\$?\" = 0 && set_ have_prod=\"true\"\n"; 
+  print $efl "test \"\$have_prod\" = \"true\" || echo \"will not setup $params[0] $params[1] -q $params[2]\"\n"; 
+  print $efl "test \"\$have_prod\" = \"true\" && setup -B $params[0] $params[1] -q $params[2] \n";
+  print $efl "unset have_prod\n"; 
+  } else {
+  print $efl "setup -B $params[0] $params[1] -q $params[2]\n";
+	#print TSET "setup -B $qlist[0][$j] $phash{$qlist[0][$j]} -q $ql \n";
+  }
+  return 0;
 }
 
 1;
