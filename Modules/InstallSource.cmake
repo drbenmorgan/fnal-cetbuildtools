d213f8b949290c59ff861895c7a3fd309b480a38
Lynn Garren <garren@fnal.gov>
2012-12-05 10:25:10 -0600

    adding install_fhicl macro

--- Modules/InstallSource.cmake
+++ Modules/InstallSource.cmake
@@ -17,11 +17,15 @@
 #   Default extensions:
 #     .sh .py .pl .rb
 #
+# install_fhicl()
+#   Install fhicl scripts in a top level fcl subdirectory
+#   Default extensions:
+#     .fcl
+#
 # The SUBDIRS option allows you to search subdirectories (e.g. a detail subdirectory)
 #
-# The EXTRAS option is intended to allow you to pick up extra files not
-# otherwise found. They should be specified by relative path (eg
-# subdir1/f1, subdir2/f2, etc.).
+# The EXTRAS option is intended to allow you to pick up extra files not otherwise found.
+# They should be specified by relative path (eg f1, subdir1/f2, etc.).
 #
 # The EXCLUDES option will exclude the specified files from the installation list.
 #
@@ -32,14 +36,25 @@
 ####################################
 # Recommended use:
 #
-# install_source( [SUBDIRS subdirs] [EXTRAS extra_files] [EXCLUDES exclusions])
-# install_source( LIST files )
+# install_source( [SUBDIRS subdirectory_list] 
+#                 [EXTRAS extra_files] 
+#                 [EXCLUDES exclusions] )
+# install_source( LIST file_list )
 #
-# install_headers( [SUBDIRS subdirs] [EXTRAS extra_files] [EXCLUDES exclusions])
-# install_headers( LIST files )
+# install_headers( [SUBDIRS subdirectory_list] 
+#                  [EXTRAS extra_files]
+#                  [EXCLUDES exclusions] )
+# install_headers( LIST file_list )
 #
-# install_scripts( [SUBDIRS subdirs] [EXTRAS extra_files]  [EXCLUDES exclusions])
-# install_scripts( LIST files )
+# install_fhicl( [SUBDIRS subdirectory_list]
+#                [EXTRAS extra_files]
+#                [EXCLUDES exclusions] )
+# install_fhicl( LIST file_list )
+#
+# install_scripts( [SUBDIRS subdirectory_list]
+#                  [EXTRAS extra_files]
+#                  [EXCLUDES exclusions] )
+# install_scripts( LIST file_list )
 #
 # set_install_root() defines PACKAGE_TOP_DIR
 #
@@ -58,82 +73,82 @@ macro( _cet_install_generated_code )
   # search for .in files
   FILE(GLOB config_source_files *.in  )
   if( config_source_files )
-    foreach( input_file ${config_source_files} )
-      STRING( REGEX REPLACE "^(${CMAKE_CURRENT_SOURCE_DIR})(.*)[.][i][n]$"  "${CMAKE_CURRENT_BINARY_DIR}\\2" output_file "${input_file}")
-      INSTALL( FILES ${output_file}
-        DESTINATION ${source_install_dir} )
-    endforeach(input_file)
+     foreach( input_file ${config_source_files} )
+	STRING( REGEX REPLACE "^(${CMAKE_CURRENT_SOURCE_DIR})(.*)[.][i][n]$"  "${CMAKE_CURRENT_BINARY_DIR}\\2" output_file "${input_file}")
+	INSTALL( FILES ${output_file}
+        	 DESTINATION ${source_install_dir} )
+     endforeach(input_file)
   endif( config_source_files )
 endmacro( _cet_install_generated_code )
 
 macro( _cet_check_build_directory )
   FILE(GLOB build_directory_files
-    ${CMAKE_CURRENT_BINARY_DIR}/[^.]*.cc
-    ${CMAKE_CURRENT_BINARY_DIR}/[^.]*.c
-    ${CMAKE_CURRENT_BINARY_DIR}/[^.]*.cpp
-    ${CMAKE_CURRENT_BINARY_DIR}/[^.]*.C
-    ${CMAKE_CURRENT_BINARY_DIR}/[^.]*.cxx
-    ${CMAKE_CURRENT_BINARY_DIR}/[^.]*.h
-    ${CMAKE_CURRENT_BINARY_DIR}/[^.]*.hh
-    ${CMAKE_CURRENT_BINARY_DIR}/[^.]*.H
-    ${CMAKE_CURRENT_BINARY_DIR}/[^.]*.hpp
-    ${CMAKE_CURRENT_BINARY_DIR}/[^.]*.icc
-    ${CMAKE_CURRENT_BINARY_DIR}/[^.]*.xml
-    ${CMAKE_CURRENT_BINARY_DIR}/[^.]*.sh
-    ${CMAKE_CURRENT_BINARY_DIR}/[^.]*.py
-    ${CMAKE_CURRENT_BINARY_DIR}/[^.]*.pl
-    ${CMAKE_CURRENT_BINARY_DIR}/[^.]*.rb
-    )
+	    ${CMAKE_CURRENT_BINARY_DIR}/[^.]*.cc
+	    ${CMAKE_CURRENT_BINARY_DIR}/[^.]*.c
+	    ${CMAKE_CURRENT_BINARY_DIR}/[^.]*.cpp
+	    ${CMAKE_CURRENT_BINARY_DIR}/[^.]*.C
+	    ${CMAKE_CURRENT_BINARY_DIR}/[^.]*.cxx
+	    ${CMAKE_CURRENT_BINARY_DIR}/[^.]*.h
+	    ${CMAKE_CURRENT_BINARY_DIR}/[^.]*.hh
+	    ${CMAKE_CURRENT_BINARY_DIR}/[^.]*.H
+	    ${CMAKE_CURRENT_BINARY_DIR}/[^.]*.hpp
+	    ${CMAKE_CURRENT_BINARY_DIR}/[^.]*.icc
+	    ${CMAKE_CURRENT_BINARY_DIR}/[^.]*.xml
+	    ${CMAKE_CURRENT_BINARY_DIR}/[^.]*.sh
+	    ${CMAKE_CURRENT_BINARY_DIR}/[^.]*.py
+	    ${CMAKE_CURRENT_BINARY_DIR}/[^.]*.pl
+	    ${CMAKE_CURRENT_BINARY_DIR}/[^.]*.rb
+	    )
   FILE(GLOB build_directory_headers
-    ${CMAKE_CURRENT_BINARY_DIR}/[^.]*.h
-    ${CMAKE_CURRENT_BINARY_DIR}/[^.]*.hh
-    ${CMAKE_CURRENT_BINARY_DIR}/[^.]*.H
-    ${CMAKE_CURRENT_BINARY_DIR}/[^.]*.hpp
-    ${CMAKE_CURRENT_BINARY_DIR}/[^.]*.icc
-    )
+	    ${CMAKE_CURRENT_BINARY_DIR}/[^.]*.h
+	    ${CMAKE_CURRENT_BINARY_DIR}/[^.]*.hh
+	    ${CMAKE_CURRENT_BINARY_DIR}/[^.]*.H
+	    ${CMAKE_CURRENT_BINARY_DIR}/[^.]*.hpp
+	    ${CMAKE_CURRENT_BINARY_DIR}/[^.]*.icc
+	    )
   if( build_directory_files )
     #message( STATUS "installing ${build_directory_files} in ${source_install_dir}")
     INSTALL( FILES ${build_directory_files}
-      DESTINATION ${source_install_dir} )
+             DESTINATION ${source_install_dir} )
   endif( build_directory_files )
   if( build_directory_headers )
     #message( STATUS "installing ${build_directory_headers} in ${source_install_dir}")
     INSTALL( FILES ${build_directory_headers}
-      DESTINATION ${header_install_dir} )
+             DESTINATION ${header_install_dir} )
   endif( build_directory_headers )
 endmacro( _cet_check_build_directory )
 
 macro( _cet_install_generated_dictionary_code )
   # check for dictionary code
   if( cet_generated_code )
-    foreach( dict ${cet_generated_code} )
-      STRING( REGEX REPLACE "^${CMAKE_CURRENT_BINARY_DIR}/(.*)$"  "\\1" dictname "${dict}")
-      # OK, this is hokey, but it works
-      set( dummy dummy_${dictname} )
-      add_custom_command(
-        OUTPUT ${dummy}
-        COMMAND ${CMAKE_COMMAND} -E copy ${dict} ${dummy}
-        DEPENDS ${dict}
+     foreach( dict ${cet_generated_code} )
+	STRING( REGEX REPLACE "^${CMAKE_CURRENT_BINARY_DIR}/(.*)$"  "\\1" dictname "${dict}")
+	# OK, this is hokey, but it works
+	set( dummy dummy_${dictname} )
+	add_custom_command(
+           OUTPUT ${dummy}
+           COMMAND ${CMAKE_COMMAND} -E copy ${dict} ${dummy}
+           DEPENDS ${dict}
         )
-      INSTALL( FILES ${dict}
-        DESTINATION ${source_install_dir} )
-    endforeach(dict)
+        INSTALL( FILES ${dict}
+                 DESTINATION ${source_install_dir} )
+     endforeach(dict)
   endif( cet_generated_code )
 endmacro( _cet_install_generated_dictionary_code )
 
 macro( _cet_install_without_list   )
   #message( STATUS "source code will be installed in ${source_install_dir}" )
   FILE(GLOB src_files
-    [^.]*.cc [^.]*.c [^.]*.cpp [^.]*.C [^.]*.cxx
-    [^.]*.h [^.]*.hh [^.]*.H [^.]*.hpp [^.]*.icc
-    [^.]*.xml [^.]*.sh [^.]*.py [^.]*.pl [^.]*.rb
-    )
+	    [^.]*.cc [^.]*.c [^.]*.cpp [^.]*.C [^.]*.cxx
+	    [^.]*.h [^.]*.hh [^.]*.H [^.]*.hpp [^.]*.icc
+	    [^.]*.xml [^.]*.sh [^.]*.py [^.]*.pl [^.]*.rb
+	    )
   if( ISRC_EXCLUDES )
     LIST( REMOVE_ITEM src_files ${ISRC_EXCLUDES} )
   endif()
   if( src_files )
     INSTALL( FILES ${src_files}
-      DESTINATION ${source_install_dir} )
+             DESTINATION ${source_install_dir} )
   endif( src_files )
   # check for generated files
   #_cet_install_generated_code()
@@ -141,20 +156,20 @@ macro( _cet_install_without_list   )
   _cet_install_generated_dictionary_code()
   # now check subdirectories
   if( ISRC_SUBDIRS )
-    foreach( sub ${ISRC_SUBDIRS} )
-      FILE(GLOB subdir_src_files
-        ${sub}/[^.]*.cc ${sub}/[^.]*.c ${sub}/[^.]*.cpp ${sub}/[^.]*.C ${sub}/[^.]*.cxx
-        ${sub}/[^.]*.h ${sub}/[^.]*.hh ${sub}/[^.]*.H ${sub}/[^.]*.hpp ${sub}/[^.]*.icc
-        ${sub}/[^.]*.xml ${sub}/[^.]*.sh ${sub}/[^.]*.py ${sub}/[^.]*.pl ${sub}/[^.]*.rb )
-      if( ISRC_EXCLUDES )
-        LIST( REMOVE_ITEM subdir_src_files ${ISRC_EXCLUDES} )
-      endif()
-      if( subdir_src_files )
-        INSTALL( FILES ${subdir_src_files}
-          DESTINATION ${source_install_dir}/${sub} )
-      endif( subdir_src_files )
-    endforeach(sub)
-    #message( STATUS "also installing in subdirectories: ${ISRC_SUBDIRS}")
+     foreach( sub ${ISRC_SUBDIRS} )
+	FILE(GLOB subdir_src_files
+        	 ${sub}/[^.]*.cc ${sub}/[^.]*.c ${sub}/[^.]*.cpp ${sub}/[^.]*.C ${sub}/[^.]*.cxx
+        	 ${sub}/[^.]*.h ${sub}/[^.]*.hh ${sub}/[^.]*.H ${sub}/[^.]*.hpp ${sub}/[^.]*.icc
+        	 ${sub}/[^.]*.xml ${sub}/[^.]*.sh ${sub}/[^.]*.py ${sub}/[^.]*.pl ${sub}/[^.]*.rb )
+	if( ISRC_EXCLUDES )
+          LIST( REMOVE_ITEM subdir_src_files ${ISRC_EXCLUDES} )
+	endif()
+	if( subdir_src_files )
+          INSTALL( FILES ${subdir_src_files}
+                   DESTINATION ${source_install_dir}/${sub} )
+	endif( subdir_src_files )
+     endforeach(sub)
+     #message( STATUS "also installing in subdirectories: ${ISRC_SUBDIRS}")
   endif( ISRC_SUBDIRS )
 endmacro( _cet_install_without_list )
 
@@ -162,7 +177,7 @@ macro( _cet_install_from_list  source_files  )
   #message( STATUS "_cet_install_from_list debug: source code list will be installed in ${source_install_dir}" )
   #message( STATUS "_cet_install_from_list debug: install list is ${source_files}")
   INSTALL( FILES ${source_files}
-    DESTINATION ${source_install_dir} )
+           DESTINATION ${source_install_dir} )
 endmacro( _cet_install_from_list )
 
 macro( _cet_install_header_without_list   )
@@ -179,19 +194,19 @@ macro( _cet_install_header_without_list   )
   if( headers )
     #message( STATUS "installing headers ${headers} in ${header_install_dir}")
     INSTALL( FILES ${headers}
-      DESTINATION ${header_install_dir} )
+             DESTINATION ${header_install_dir} )
   endif( headers )
   # now check subdirectories
   if( IHDR_SUBDIRS )
     foreach( sub ${IHDR_SUBDIRS} )
       FILE(GLOB subdir_headers
-        ${sub}/[^.]*.h ${sub}/[^.]*.hh ${sub}/[^.]*.H ${sub}/[^.]*.hpp ${sub}/[^.]*.icc  )
+                ${sub}/[^.]*.h ${sub}/[^.]*.hh ${sub}/[^.]*.H ${sub}/[^.]*.hpp ${sub}/[^.]*.icc  )
       if(IHDR_EXCLUDES)
         LIST( REMOVE_ITEM subdir_headers ${IHDR_EXCLUDES} )
       endif()
       if( subdir_headers )
         INSTALL( FILES ${subdir_headers}
-          DESTINATION ${header_install_dir}/${sub} )
+                 DESTINATION ${header_install_dir}/${sub} )
       endif( subdir_headers )
     endforeach(sub)
     #message( STATUS "also installing in subdirectories: ${IHDR_SUBDIRS}")
@@ -199,10 +214,10 @@ macro( _cet_install_header_without_list   )
 endmacro( _cet_install_header_without_list )
 
 macro( _cet_install_header_from_list header_list  )
-  message( STATUS "_cet_install_header_from_list debug: source code list will be installed in ${header_install_dir}" )
-  message( STATUS "_cet_install_header_from_list debug: install list is ${header_list}")
+  ##message( STATUS "_cet_install_header_from_list debug: source code list will be installed in ${header_install_dir}" )
+  ##message( STATUS "_cet_install_header_from_list debug: install list is ${header_list}")
   INSTALL( FILES ${header_list}
-    DESTINATION ${header_install_dir} )
+           DESTINATION ${header_install_dir} )
 endmacro( _cet_install_header_from_list )
 
 macro( _cet_install_script_without_list   )
@@ -214,39 +229,65 @@ macro( _cet_install_script_without_list   )
   if( scripts )
     #message( STATUS "installing scripts ${scripts} in ${script_install_dir}")
     INSTALL ( PROGRAMS ${scripts}
-      DESTINATION ${script_install_dir} )
+              DESTINATION ${script_install_dir} )
   endif( scripts )
   # now check subdirectories
   if( IS_SUBDIRS )
     foreach( sub ${IS_SUBDIRS} )
       FILE(GLOB subdir_scripts
-        ${sub}/[^.]*.sh ${sub}/[^.]*.py ${sub}/[^.]*.pl ${sub}/[^.]*.rb )
+                ${sub}/[^.]*.sh ${sub}/[^.]*.py ${sub}/[^.]*.pl ${sub}/[^.]*.rb )
       if( IS_EXCLUDES )
         LIST( REMOVE_ITEM subdir_scripts ${IS_EXCLUDES} )
       endif()
       if( subdir_scripts )
         INSTALL ( PROGRAMS ${subdir_scripts}
-          DESTINATION ${script_install_dir} )
+                  DESTINATION ${script_install_dir} )
       endif( subdir_scripts )
     endforeach(sub)
-    #message( STATUS "also installing in subdirectories: ${IS_SUBDIRS}")
   endif( IS_SUBDIRS )
 endmacro( _cet_install_script_without_list )
+
+macro( _cet_install_fhicl_without_list   )
+  #message( STATUS "fhicl scripts will be installed in ${fhicl_install_dir}" )
+  FILE(GLOB fcl_files [^.]*.fcl )
+  if( IFCL_EXCLUDES )
+    LIST( REMOVE_ITEM fcl_files ${IFCL_EXCLUDES} )
+  endif()
+  if( fcl_files )
+    #message( STATUS "installing fhicl files ${fcl_files} in ${fhicl_install_dir}")
+    INSTALL ( PROGRAMS ${fcl_files}
+              DESTINATION ${fhicl_install_dir} )
+  endif( fcl_files )
+  # now check subdirectories
+  if( IFCL_SUBDIRS )
+    foreach( sub ${IFCL_SUBDIRS} )
+      FILE(GLOB subdir_fcl_files
+                ${sub}/[^.]*.fcl )
+      if( IFCL_EXCLUDES )
+        LIST( REMOVE_ITEM subdir_fcl_files ${IFCL_EXCLUDES} )
+      endif()
+      if( subdir_fcl_files )
+        INSTALL ( PROGRAMS ${subdir_fcl_files}
+                  DESTINATION ${fhicl_install_dir} )
+      endif( subdir_fcl_files )
+    endforeach(sub)
+  endif( IFCL_SUBDIRS )
+endmacro( _cet_install_fhicl_without_list )
 
 macro( install_source   )
   cet_parse_args( ISRC "SUBDIRS;LIST;EXTRAS;EXCLUDES" "" ${ARGN})
   #message( STATUS "install_source: PACKAGE_TOP_DIRECTORY is ${PACKAGE_TOP_DIRECTORY}")
   if( PACKAGE_TOP_DIRECTORY )
-    STRING( REGEX REPLACE "^${PACKAGE_TOP_DIRECTORY}(.*)" "\\1" CURRENT_SUBDIR "${CMAKE_CURRENT_SOURCE_DIR}" )
+     STRING( REGEX REPLACE "^${PACKAGE_TOP_DIRECTORY}(.*)" "\\1" CURRENT_SUBDIR "${CMAKE_CURRENT_SOURCE_DIR}" )
   else()
-    STRING( REGEX REPLACE "^${CMAKE_SOURCE_DIR}(.*)" "\\1" CURRENT_SUBDIR "${CMAKE_CURRENT_SOURCE_DIR}" )
+     STRING( REGEX REPLACE "^${CMAKE_SOURCE_DIR}(.*)" "\\1" CURRENT_SUBDIR "${CMAKE_CURRENT_SOURCE_DIR}" )
   endif()
   set(source_install_dir ${product}/${version}/source/${CURRENT_SUBDIR} )
   #message( STATUS "install_source: source code will be installed in ${source_install_dir}" )
   if( ISRC_LIST )
     if( ISRC_SUBDIRS )
       message( FATAL_ERROR
-        "ERROR: call install_source with EITHER LIST or SUBDIRS but not both")
+               "ERROR: call install_source with EITHER LIST or SUBDIRS but not both")
     endif( ISRC_SUBDIRS )
     _cet_install_from_list("${ISRC_LIST}")
   else()
@@ -269,7 +310,7 @@ macro( install_headers   )
   if( IHDR_LIST )
     if( IHDR_SUBDIRS )
       message( FATAL_ERROR
-        "ERROR: call install_headers with EITHER LIST or SUBDIRS but not both")
+               "ERROR: call install_headers with EITHER LIST or SUBDIRS but not both")
     endif( IHDR_SUBDIRS )
     _cet_install_header_from_list("${IHDR_LIST}")
   else()
@@ -287,15 +328,35 @@ macro( install_scripts   )
   if( IS_LIST )
     if( IS_SUBDIRS )
       message( FATAL_ERROR
-        "ERROR: call install_scripts with EITHER LIST or SUBDIRS but not both")
+               "ERROR: call install_scripts with EITHER LIST or SUBDIRS but not both")
     endif( IS_SUBDIRS )
     INSTALL ( PROGRAMS  ${IS_LIST}
-      DESTINATION ${script_install_dir} )
+              DESTINATION ${script_install_dir} )
   else()
     if( IS_EXTRAS )
       INSTALL ( PROGRAMS  ${IS_EXTRAS}
-        DESTINATION ${script_install_dir} )
+                DESTINATION ${script_install_dir} )
     endif( IS_EXTRAS )
     _cet_install_script_without_list()
   endif()
 endmacro( install_scripts )
+
+macro( install_fhicl   )
+  cet_parse_args( IFCL "SUBDIRS;LIST;EXTRAS;EXCLUDES" "" ${ARGN})
+  set(fhicl_install_dir ${product}/${version}/fcl )
+  #message( STATUS "install_fhicl: fhicl scripts will be installed in ${fhicl_install_dir}" )
+  if( IFCL_LIST )
+    if( IFCL_SUBDIRS )
+      message( FATAL_ERROR
+               "ERROR: call install_fhicl with EITHER LIST or SUBDIRS but not both")
+    endif( IFCL_SUBDIRS )
+    INSTALL ( PROGRAMS  ${IFCL_LIST}
+              DESTINATION ${fhicl_install_dir} )
+  else()
+    if( IFCL_EXTRAS )
+      INSTALL ( PROGRAMS  ${IFCL_EXTRAS}
+                DESTINATION ${fhicl_install_dir} )
+    endif( IFCL_EXTRAS )
+    _cet_install_fhicl_without_list()
+  endif()
+endmacro( install_fhicl )
