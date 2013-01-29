cddc6280915d18fef19892195e13f01b59c2e8ca
Lynn Garren <garren@fnal.gov>
2013-01-29 16:24:15 -0600

    remove debug messages

--- Modules/EnsureOutOfSourceBuild.cmake
+++ Modules/EnsureOutOfSourceBuild.cmake
@@ -4,33 +4,23 @@
 
 macro (cet_ensure_out_of_source_build)
 
-message(STATUS "cet_ensure_out_of_source_build: CMAKE_SOURCE_DIR is ${CMAKE_SOURCE_DIR}")
-message(STATUS "cet_ensure_out_of_source_build: CMAKE_BINARY_DIR is ${CMAKE_BINARY_DIR}")
+##message(STATUS "cet_ensure_out_of_source_build: CMAKE_SOURCE_DIR is ${CMAKE_SOURCE_DIR}")
+##message(STATUS "cet_ensure_out_of_source_build: CMAKE_BINARY_DIR is ${CMAKE_BINARY_DIR}")
 
   string(COMPARE EQUAL "${CMAKE_SOURCE_DIR}" "${CMAKE_BINARY_DIR}" in_source)
   string( REGEX MATCH "${CMAKE_SOURCE_DIR}/" maybe_in_source_subdir "${CMAKE_BINARY_DIR}")
 
-  message(STATUS "cet_ensure_out_of_source_build: ${in_source} and ${maybe_in_source_subdir}")
-  if (in_source )
-  message(STATUS "in source")
-  endif ()
+  ##message(STATUS "cet_ensure_out_of_source_build: ${in_source} and ${maybe_in_source_subdir}")
   if( maybe_in_source_subdir )
     execute_process( COMMAND /bin/pwd
 		     OUTPUT_VARIABLE thisdir
 		     OUTPUT_STRIP_TRAILING_WHITESPACE )
-    message(STATUS "/bin/pwd returns ${thisdir}")
+    ##message(STATUS "/bin/pwd returns ${thisdir}")
     string( REGEX MATCH "${CMAKE_SOURCE_DIR}/" in_source_subdir "${thisdir}")
-    message(STATUS "regex match returns --${in_source_subdir}--")
+    ##message(STATUS "regex match returns --${in_source_subdir}--")
   endif ()
   
-  message(STATUS "cet_ensure_out_of_source_build: in_source_subdir is ${in_source_subdir}")
-  if (in_source )
-    message(STATUS "in source")
-  endif ()
-  if (in_source_subdir)
-     message(STATUS "in source subdirectory")
-  endif ()
-
+  ##message(STATUS "cet_ensure_out_of_source_build: in_source_subdir is ${in_source_subdir}")
   if (in_source OR in_source_subdir)
   message(FATAL_ERROR "
 ERROR: In source builds of this project are not allowed.
