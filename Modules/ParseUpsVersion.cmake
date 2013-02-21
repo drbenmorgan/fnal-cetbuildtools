04d9651ab9a4dd0d444aa231c2f4fa9e78b3758a
Lynn Garren <garren@fnal.gov>
2013-02-21 10:09:19 -0600

    cleanup

--- Modules/ParseUpsVersion.cmake
+++ Modules/ParseUpsVersion.cmake
@@ -11,7 +11,7 @@ macro( parse_ups_version UPS_VERSION )
   STRING( REGEX REPLACE "^[v](.*)[_](.*)[_](.*)$" "\\1" VMAJ "${UPS_VERSION}" )
   STRING( REGEX REPLACE "^[v](.*)[_](.*)[_](.*)$" "\\2" VMIN "${UPS_VERSION}" )
   STRING( REGEX REPLACE "^[v](.*)[_](.*)[_](.*)$" "\\3" VPRJ "${UPS_VERSION}" )
-  message(STATUS "version parses to ${VMAJ}.${VMIN}.${VPRJ}" )
+  #message(STATUS "version parses to ${VMAJ}.${VMIN}.${VPRJ}" )
 
 endmacro( parse_ups_version )
 
@@ -22,7 +22,7 @@ macro( set_version_from_ups UPS_VERSION )
   set( VERSION_MAJOR ${VMAJ} CACHE STRING "Package major version" FORCE)
   set( VERSION_MINOR ${VMIN} CACHE STRING "Package minor version" FORCE )
   set( VERSION_PATCH ${VPRJ} CACHE STRING "Package patch version" FORCE )
-  message(STATUS "project version is ${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}" )
+  #message(STATUS "project version is ${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}" )
 
 endmacro( set_version_from_ups )
 
@@ -32,6 +32,6 @@ macro( set_dot_version PRODUCTNAME UPS_VERSION )
   STRING( REGEX REPLACE "_" "." VDOT "${UPS_VERSION}" )
   ##message(STATUS "temp version is ${VDOT}" )
   STRING( REGEX REPLACE "^[v]" "" ${PRODUCTNAME_UC}_DOT_VERSION "${VDOT}" )
-  message(STATUS "${PRODUCTNAME_UC}_DOT_VERSION version is ${${PRODUCTNAME_UC}_DOT_VERSION}" )
+  #message(STATUS "${PRODUCTNAME_UC}_DOT_VERSION version is ${${PRODUCTNAME_UC}_DOT_VERSION}" )
 
 endmacro( set_dot_version )
