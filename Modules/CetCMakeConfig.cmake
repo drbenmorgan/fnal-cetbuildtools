7a8531aebb66298aa5cb2064bca0676f4da06b70
Lynn Garren <garren@fnal.gov>
2013-04-17 10:06:45 -0500

    Use case instead of - in config files

--- Modules/CetCMakeConfig.cmake
+++ Modules/CetCMakeConfig.cmake
@@ -5,7 +5,7 @@
 # noarch is recognized, others are used at your own discretion
 #
 # cet_cmake_config( [NO_FLAVOR] )
-#   build and install PACKAGE-config-cmake and PACKAGE-config-version.cmake
+#   build and install PackageConfig.cmake and PackageConfigVersion.cmake
 #   these files are installed in ${flavorqual_dir}/lib/PACKAGE/cmake
 #   if NO_FLAVOR is specified, the files are installed under ${product}/${version}/include
 
@@ -40,18 +40,18 @@ macro( cet_cmake_config  )
  
   configure_package_config_file( 
              ${CMAKE_CURRENT_SOURCE_DIR}/product-config.cmake.in
-             ${CMAKE_CURRENT_BINARY_DIR}/${product}-config.cmake 
+             ${CMAKE_CURRENT_BINARY_DIR}/${product}Config.cmake 
 	     INSTALL_DESTINATION ${distdir} )
 
   # allowed COMPATIBILITY values are:
   # AnyNewerVersion ExactVersion SameMajorVersion
   write_basic_package_version_file(
-             ${CMAKE_CURRENT_BINARY_DIR}/${product}-config-version.cmake
+             ${CMAKE_CURRENT_BINARY_DIR}/${product}ConfigVersion.cmake
 	     VERSION ${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}
 	     COMPATIBILITY AnyNewerVersion )
 
-  install( FILES ${CMAKE_CURRENT_BINARY_DIR}/${product}-config.cmake
-        	 ${CMAKE_CURRENT_BINARY_DIR}/${product}-config-version.cmake
+  install( FILES ${CMAKE_CURRENT_BINARY_DIR}/${product}Config.cmake
+        	 ${CMAKE_CURRENT_BINARY_DIR}/${product}ConfigVersion.cmake
            DESTINATION ${distdir} )
 
 endmacro( cet_cmake_config )
