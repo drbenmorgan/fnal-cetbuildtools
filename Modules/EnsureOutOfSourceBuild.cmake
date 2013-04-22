71af20e8c7b0badb1f3c1b3e1ec3c3da408c048d
Chris Green <artists@fnal.gov>
2013-04-22 11:20:03 -0500

    Cope with the possibility that /bin/pwd does not understand -P.

--- Modules/EnsureOutOfSourceBuild.cmake
+++ Modules/EnsureOutOfSourceBuild.cmake
@@ -2,21 +2,44 @@
 # cet_ensure_out_of_source_build()
 #
 
+execute_process(COMMAND /bin/pwd -P
+  WORKING_DIRECTORY /
+  OUTPUT_QUIET
+  ERROR_QUIET
+  RESULT_VARIABLE _cet_pwd_status
+  )
+
+if (_cet_pwd_status EQUAL 0)
+  set (_cet_pwd_P_arg "-P" CACHE "/bin/pwd args" INTERNAL )
+else()
+  set (_cet_pwd_P_arg "" CACHE "/bin/pwd args" INTERNAL )
+endif()
+
+function (_cet_real_dir INPUT_DIR OUTPUT_VAR)
+  execute_process(COMMAND /bin/pwd ${_cet_pwd_P_arg}
+    WORKING_DIRECTORY "${INPUT_DIR}"
+    OUTPUT_VARIABLE result
+    ERROR_QUIET
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+  set (${OUTPUT_VAR} "${result}" PARENT_SCOPE)
+endfunction()
+
 macro (cet_ensure_out_of_source_build)
 
 ##message(STATUS "cet_ensure_out_of_source_build: CMAKE_SOURCE_DIR is ${CMAKE_SOURCE_DIR}")
 ##message(STATUS "cet_ensure_out_of_source_build: CMAKE_BINARY_DIR is ${CMAKE_BINARY_DIR}")
 
-  string(COMPARE EQUAL "${CMAKE_SOURCE_DIR}" "${CMAKE_BINARY_DIR}" in_source)
-  string( REGEX MATCH "${CMAKE_SOURCE_DIR}/" maybe_in_source_subdir "${CMAKE_BINARY_DIR}")
+  _cet_real_dir("${CMAKE_SOURCE_DIR}" cet_source_real)
+  _cet_real_dir("${CMAKE_BINARY_DIR}" cet_build_real)
+  string(COMPARE EQUAL "${cet_source_real}" "${cet_build_real}" in_source)
+  string( REGEX MATCH "${cet_source_real}/" maybe_in_source_subdir "${cet_build_real}")
 
   ##message(STATUS "cet_ensure_out_of_source_build: ${in_source} and ${maybe_in_source_subdir}")
   if( maybe_in_source_subdir )
-    execute_process( COMMAND /bin/pwd -P
-		     OUTPUT_VARIABLE thisdir
-		     OUTPUT_STRIP_TRAILING_WHITESPACE )
+    _cet_real_dir("${CMAKE_CURRENT_BINARY_DIR}" thisdir)
     ##message(STATUS "/bin/pwd returns ${thisdir}")
-    string( REGEX MATCH "${CMAKE_SOURCE_DIR}/" in_source_subdir "${thisdir}")
+    string( REGEX MATCH "${cet_source_real}/" in_source_subdir "${thisdir}")
     ##message(STATUS "regex match returns --${in_source_subdir}--")
   endif ()
   
