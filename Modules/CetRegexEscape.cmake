002770d587cfea8c18a397d454d70f7dc776569e
Chris Green <greenc@fnal.gov>
2014-04-03 16:33:28 -0500

    Split armoring functionality into a separate function.

--- Modules/CetRegexEscape.cmake
+++ Modules/CetRegexEscape.cmake
@@ -4,21 +4,39 @@
 #   Escape the provided string to prevent interpretation by the CMake
 #   regex engine.
 #
-# The optional <num> argument indicates the expected interpolation level
-# for the resulting string. The default is 1. Every time the string is
-# expected to be passed to a function or macro, increase <num> to ensure
-# that "\" are correctly handled.
+# The result of escaping characters which would be interpreted by
+# CMake's regex engine is passed through cet_armor_string if <num> is
+# specified and non-zero.
 #
 ########################################################################
-function(cet_regex_escape val var)    
-  string(REGEX REPLACE "(\\.|\\||\\^|\\$|\\*|\\(|\\)|\\[|\\]|\\+)" "\\\\\\1" tmp "${val}")
-  string(REGEX REPLACE "/+" "/" tmp "${tmp}")
-  if (${ARGN})
-    set(count ${ARGN})
-    while (count GREATER 1) # Extra escapes for passing to macros.
-      string(REPLACE "\\" "\\\\" tmp "${tmp}")
-      math(EXPR count "${count} - 1")
-    endwhile()
+# cet_armor_string(<val> <var> <num>)
+#
+#    Armor the instances of "\" in the string aginst being passed to a
+#    macro (and therefore being interpolated).
+#
+# The <num> argument indicates the expected interpolation level for the
+# resulting string (0 is a NOP). Every time the string is expected to be
+# passed to a macro, increase <num> to ensure that "\" are correctly
+# handled. This is not necessary for a function.
+#
+########################################################################
+
+function(cet_regex_escape val var)
+  string(REGEX REPLACE "(\\.|\\||\\^|\\$|\\*|\\(|\\)|\\[|\\]|\\+)" "\\\\\\1" val "${val}")
+  string(REGEX REPLACE "/+" "/" val "${val}")
+  if (ARGN)
+    list(GET ARGN 0 count)
   endif()
-  set(${var} "${tmp}" PARENT_SCOPE)
+  if (count)
+    cet_armor_string("${val}" val ${count})
+  endif()
+  set(${var} "${val}" PARENT_SCOPE)
+endfunction()
+
+function(cet_armor_string val var count)
+  while (count GREATER 0) # Extra escapes for passing to macros.
+    string(REPLACE "\\" "\\\\" val "${val}")
+    math(EXPR count "${count} - 1")
+  endwhile()
+  set(${var} "${val}" PARENT_SCOPE)
 endfunction()
