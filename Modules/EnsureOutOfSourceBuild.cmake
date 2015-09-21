0bb458aa0365b5adf021f53614b625a0fc9f4232
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2015-09-21 12:55:02 +0100

    Pure CMake implementation of ensureoutofsourcebuild
    Existing implementation uses system calls to `pwd`, but CMake knows
    where it is running and the location of the source directory.
    
    Replace ensureoutofsourcebuild macro with function, simplify
    implementation to require that build directory not be any subdirectory
    of the source tree.

--- Modules/EnsureOutOfSourceBuild.cmake
+++ Modules/EnsureOutOfSourceBuild.cmake
@@ -1,50 +1,11 @@
 # Throw a fatal error if cmake is invoked from within the source code directory tree
 # cet_ensure_out_of_source_build()
 #
+function(cet_ensure_out_of_source_build)
+  get_filename_component(ACTUAL_CMAKE_BINARY_DIR "${CMAKE_BINARY_DIR}" REALPATH)
+  string(REPLACE "${CMAKE_SOURCE_DIR}" "isasubdir" IS_INSOURCE "${ACTUAL_CMAKE_BINARY_DIR}")
 
-execute_process(COMMAND /bin/pwd -P
-  WORKING_DIRECTORY /
-  OUTPUT_QUIET
-  ERROR_QUIET
-  RESULT_VARIABLE _cet_pwd_status
-  )
-
-if (_cet_pwd_status EQUAL 0)
-  set (_cet_pwd_P_arg "-P" CACHE "/bin/pwd args" INTERNAL )
-else()
-  set (_cet_pwd_P_arg "" CACHE "/bin/pwd args" INTERNAL )
-endif()
-
-function (_cet_real_dir INPUT_DIR OUTPUT_VAR)
-  execute_process(COMMAND /bin/pwd ${_cet_pwd_P_arg}
-    WORKING_DIRECTORY "${INPUT_DIR}"
-    OUTPUT_VARIABLE result
-    ERROR_QUIET
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-    )
-  set (${OUTPUT_VAR} "${result}" PARENT_SCOPE)
-endfunction()
-
-macro (cet_ensure_out_of_source_build)
-
-##message(STATUS "cet_ensure_out_of_source_build: CMAKE_SOURCE_DIR is ${CMAKE_SOURCE_DIR}")
-##message(STATUS "cet_ensure_out_of_source_build: CMAKE_BINARY_DIR is ${CMAKE_BINARY_DIR}")
-
-  _cet_real_dir("${CMAKE_SOURCE_DIR}" cet_source_real)
-  _cet_real_dir("${CMAKE_BINARY_DIR}" cet_build_real)
-  string(COMPARE EQUAL "${cet_source_real}" "${cet_build_real}" in_source)
-  string( REGEX MATCH "${cet_source_real}/" maybe_in_source_subdir "${cet_build_real}")
-
-  ##message(STATUS "cet_ensure_out_of_source_build: ${in_source} and ${maybe_in_source_subdir}")
-  if( maybe_in_source_subdir )
-    _cet_real_dir("${CMAKE_CURRENT_BINARY_DIR}" thisdir)
-    ##message(STATUS "/bin/pwd returns ${thisdir}")
-    string( REGEX MATCH "${cet_source_real}/" in_source_subdir "${thisdir}")
-    ##message(STATUS "regex match returns --${in_source_subdir}--")
-  endif ()
-  
-  ##message(STATUS "cet_ensure_out_of_source_build: in_source_subdir is ${in_source_subdir}")
-  if (in_source OR in_source_subdir)
+  if (IS_INSOURCE)
   message(FATAL_ERROR "
 ERROR: In source builds of this project are not allowed.
 A separate build directory is required.
@@ -53,7 +14,6 @@ Also note that cmake has just added files to your source code directory.
 We suggest getting a new copy of the source code.
 Otherwise, delete `CMakeCache.txt' and the directory `CMakeFiles'.
   ")
-  endif ()
-
-endmacro (cet_ensure_out_of_source_build)
+  endif()
+endfunction()
 
