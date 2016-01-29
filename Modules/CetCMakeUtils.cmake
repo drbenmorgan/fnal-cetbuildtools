a81f87e7fc18283e9474850ef7045a4ce22bc5af
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2016-01-29 13:51:04 +0000

    Whitespace/Indent/CMake style cleanup

--- Modules/CetCMakeUtils.cmake
+++ Modules/CetCMakeUtils.cmake
@@ -15,64 +15,72 @@
 # cet_find_cetskelgen()
 #
 
+# - Initialization
 macro(_cet_init_config_var)
   # initialize cmake config file fragments
   set(CONFIG_FIND_UPS_COMMANDS "
 ## find_ups_product directives
-## remember that these are minimum required versions" 
-      CACHE STRING "UPS product directives for config" FORCE)
+## remember that these are minimum required versions"
+    CACHE STRING "UPS product directives for config"
+    FORCE
+    )
   set(CONFIG_FIND_LIBRARY_COMMANDS "
-## find_library directives" 
-      CACHE STRING "find_library directives for config" FORCE)
-  set(CONFIG_LIBRARY_LIST "" CACHE INTERNAL "libraries created by this package" )
-  set(CONFIG_PM_LIST "" CACHE INTERNAL "perl libraries created by this package" )
-  set(CONFIG_PERL_PLUGIN_LIST "" CACHE INTERNAL "perl plugin libraries created by this package" )
-  set(CONFIG_PM_VERSION "" CACHE INTERNAL "just for PluginVersionInfo.pm" )
+## find_library directives"
+    CACHE STRING "find_library directives for config"
+    FORCE
+    )
+  set(CONFIG_LIBRARY_LIST "" CACHE INTERNAL "libraries created by this package")
+  set(CONFIG_PM_LIST "" CACHE INTERNAL "perl libraries created by this package")
+  set(CONFIG_PERL_PLUGIN_LIST "" CACHE INTERNAL "perl plugin libraries created by this package")
+  set(CONFIG_PM_VERSION "" CACHE INTERNAL "just for PluginVersionInfo.pm")
+
   # we use cet_product_list to make sure there is only one find_ups_product call
   set(cet_product_list "" CACHE STRING "list of ups products" FORCE)
   set(cet_find_library_list "" CACHE STRING "list of calls to cet_find_library" FORCE)
-endmacro(_cet_init_config_var)
+endmacro()
 
+# - Add lib...
 macro(cet_add_to_library_list libname)
-     # add to library list for package configure file
-     set(CONFIG_LIBRARY_LIST ${CONFIG_LIBRARY_LIST} ${libname}
-	 CACHE INTERNAL "libraries created by this package" )
-endmacro(cet_add_to_library_list)
+  # add to library list for package configure file
+  set(CONFIG_LIBRARY_LIST ${CONFIG_LIBRARY_LIST} ${libname}
+    CACHE INTERNAL "libraries created by this package"
+    )
+endmacro()
 
+# - find_library wrapper - basically stores call then forwards
 macro(cet_find_library)
-  STRING( REGEX REPLACE ";" " " find_library_commands "${ARGN}" )
-  #message(STATUS "cet_find_library debug: find_library_commands ${find_library_commands}" )
-
-  #message(STATUS "cet_find_library debug: cet_find_library_list ${cet_find_library_list}")
+  string(REGEX REPLACE ";" " " find_library_commands "${ARGN}")
   list(FIND cet_find_library_list ${ARGV2} found_library_match)
-  if( ${found_library_match} LESS 0 )
-    set(cet_find_library_list ${ARGV2} ${cet_find_library_list} )
+
+  if(${found_library_match} LESS 0)
+    set(cet_find_library_list ${ARGV2} ${cet_find_library_list})
     # add to library list for package configure file
     set(CONFIG_FIND_LIBRARY_COMMANDS "${CONFIG_FIND_LIBRARY_COMMANDS}
     if( NOT ${ARGV0} )
       cet_find_library( ${find_library_commands} )
-    endif()" )
+    endif()"
+    )
   endif()
 
   # call find_library
-  find_library( ${ARGN} )
+  find_library(${ARGN})
 endmacro(cet_find_library)
 
-macro( cet_find_cetskelgen )
-   set( CETLIB_FQ_DIR $ENV{CETLIB_FQ_DIR} )
-   if( ${product} MATCHES "cetlib" )
-       # building cetlib - use our copy
-       #message(STATUS "cet_find_cetskelgen: looking in ${PROJECT_SOURCE_DIR}/perllib")
-       FIND_PROGRAM( CETSKELGEN_EXE cetskelgen
-                     ${PROJECT_SOURCE_DIR}/perllib  )
-   elseif( NOT CETLIB_FQ_DIR )
-       FIND_PROGRAM( CETSKELGEN_EXE cetskelgen )
-   else()
-       FIND_PROGRAM( CETSKELGEN_EXE cetskelgen
-                     ${CETLIB_FQ_DIR}/bin  )
-   endif ()
-   if( NOT CETSKELGEN_EXE )
-       message(FATAL_ERROR "Can't find cetskelgen")
-   endif()
-   message(STATUS "cet_find_cetskelgen: CETSKELGEN_EXE: ${CETSKELGEN_EXE}")
-endmacro( cet_find_cetskelgen )
+# - Again, a dependence on cetlib!!
+macro(cet_find_cetskelgen)
+  set(CETLIB_FQ_DIR $ENV{CETLIB_FQ_DIR})
+  if(${product} MATCHES "cetlib" )
+    # building cetlib - use our copy
+    find_program(CETSKELGEN_EXE cetskelgen ${PROJECT_SOURCE_DIR}/perllib)
+  elseif(NOT CETLIB_FQ_DIR)
+    find_program(CETSKELGEN_EXE cetskelgen)
+  else()
+    find_program(CETSKELGEN_EXE cetskelgen ${CETLIB_FQ_DIR}/bin)
+  endif()
+
+  if(NOT CETSKELGEN_EXE)
+    message(FATAL_ERROR "Can't find cetskelgen")
+  endif()
+  message(STATUS "cet_find_cetskelgen: CETSKELGEN_EXE: ${CETSKELGEN_EXE}")
+endmacro()
+
