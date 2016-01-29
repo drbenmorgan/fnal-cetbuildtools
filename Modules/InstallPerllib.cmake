95d94369ad95e20ca0056fe20394f7df90fb2058
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2016-01-29 13:43:41 +0000

    Whitespace/Indent/CMake style cleanup

--- Modules/InstallPerllib.cmake
+++ Modules/InstallPerllib.cmake
@@ -27,129 +27,130 @@
 
 include(CetCurrentSubdir)
 
-macro( _cet_perl_plugin_version )
+# - ??
+macro(_cet_perl_plugin_version)
   configure_file($ENV{CETLIB_DIR}/perllib/PluginVersionInfo.pm.in
     ${CMAKE_CURRENT_BINARY_DIR}/${product}/PluginVersionInfo.pm
-    @ONLY)
-    set(CONFIG_PM_VERSION "PluginVersionInfo.pm"
-	   CACHE INTERNAL "just for PluginVersionInfo.pm" )
-  install(FILES ${CMAKE_CURRENT_BINARY_DIR}/${product}/PluginVersionInfo.pm
-    DESTINATION ${perllib_install_dir}/${product}/)
-endmacro( _cet_perl_plugin_version )
+    @ONLY
+    )
 
-macro( _cet_copy_perllib )
+  set(CONFIG_PM_VERSION "PluginVersionInfo.pm" CACHE INTERNAL "just for PluginVersionInfo.pm")
+  install(FILES ${CMAKE_CURRENT_BINARY_DIR}/${product}/PluginVersionInfo.pm
+    DESTINATION ${perllib_install_dir}/${product}/
+    )
+endmacro()
+
+# - ??
+macro(_cet_copy_perllib)
   cmake_parse_arguments( CPPRL "" "SUBDIR;WORKING_DIRECTORY" "LIST" ${ARGN})
-  set( mrb_build_dir $ENV{MRB_BUILDDIR} )
-  #message(STATUS "_cet_copy_perllib: copying to mrb ${mrb_build_dir}/${product}/${prlpathname} or cet ${CETPKG_BUILD}/${prlpathname}")
-  if( mrb_build_dir )
-    set( perllibbuiildpath ${mrb_build_dir}/${product}/${prlpathname} )
+
+  set(mrb_build_dir $ENV{MRB_BUILDDIR})
+  if(mrb_build_dir)
+    set(perllibbuiildpath ${mrb_build_dir}/${product}/${prlpathname})
   else()
-    set( perllibbuiildpath ${CETPKG_BUILD}/${prlpathname} )
+    set(perllibbuiildpath ${CETPKG_BUILD}/${prlpathname})
   endif()
-  if( CPPRL_SUBDIR )
-    set( perllibbuiildpath "${perllibbuiildpath}/${CPPRL_SUBDIR}" )
-  endif( CPPRL_SUBDIR )
-  if (CPPRL_WORKING_DIRECTORY)
+
+  if(CPPRL_SUBDIR)
+    set(perllibbuiildpath "${perllibbuiildpath}/${CPPRL_SUBDIR}")
+  endif()
+
+  if(CPPRL_WORKING_DIRECTORY)
     cet_copy(${CPPRL_LIST} DESTINATION "${perllibbuiildpath}" WORKING_DIRECTORY "${CPPRL_WORKING_DIRECTORY}")
   else()
     cet_copy(${CPPRL_LIST} DESTINATION "${perllibbuiildpath}")
   endif()
-  #message(STATUS "_cet_copy_perllib: copying to ${perllibbuiildpath}")
-endmacro( _cet_copy_perllib )
+endmacro()
 
+# - ??
 macro(_cet_add_to_pm_list libname)
-     # add to perl library list for package configure file
-     set(CONFIG_PM_LIST ${CONFIG_PM_LIST} ${libname}
-	 CACHE INTERNAL "perl libraries installed by this package" )
-endmacro(_cet_add_to_pm_list)
+  # add to perl library list for package configure file
+  set(CONFIG_PM_LIST ${CONFIG_PM_LIST} ${libname} CACHE INTERNAL "perl libraries installed by this package")
+endmacro()
 
+# - ??
 macro(_cet_add_to_perl_plugin_list libname)
-     # add to perl library list for package configure file
-     set(CONFIG_PERL_PLUGIN_LIST ${CONFIG_PERL_PLUGIN_LIST} ${libname}
-	 CACHE INTERNAL "perl plugin libraries installed by this package" )
-endmacro(_cet_add_to_perl_plugin_list)
+  # add to perl library list for package configure file
+  set(CONFIG_PERL_PLUGIN_LIST ${CONFIG_PERL_PLUGIN_LIST} ${libname} CACHE INTERNAL "perl plugin libraries installed by this package")
+endmacro()
 
-macro( _cet_perllib_config_setup  )
-  if( ${CURRENT_SUBDIR_NAME} MATCHES "CetSkelPlugins" )
-    foreach( pmfile ${ARGN} )
-      get_filename_component( pmfilename "${pmfile}" NAME )
-      _cet_add_to_perl_plugin_list( ${CURRENT_SUBDIR}/${pmfilename} )
-    endforeach( pmfile )
+# - ??
+macro(_cet_perllib_config_setup)
+  if(${CURRENT_SUBDIR_NAME} MATCHES "CetSkelPlugins")
+    foreach(pmfile ${ARGN})
+      get_filename_component(pmfilename "${pmfile}" NAME)
+      _cet_add_to_perl_plugin_list(${CURRENT_SUBDIR}/${pmfilename})
+    endforeach()
   else()
-    foreach( pmfile ${ARGN} )
-      get_filename_component( pmfilename "${pmfile}" NAME )
-      _cet_add_to_pm_list( ${CURRENT_SUBDIR}/${pmfilename} )
-    endforeach( pmfile )
+    foreach(pmfile ${ARGN})
+      get_filename_component(pmfilename "${pmfile}" NAME)
+      _cet_add_to_pm_list(${CURRENT_SUBDIR}/${pmfilename})
+    endforeach()
   endif()
-endmacro( _cet_perllib_config_setup )
+endmacro()
 
-macro( _cet_install_perllib_without_list   )
-  #message( STATUS "_cet_install_perllib_without_list: perl lib scripts will be installed in ${perllib_install_dir}" )
-  FILE(GLOB prl_files [^.]*.pm )
-  FILE(GLOB prl_files2 [^.]*.pm README )
-  if( IPRL_EXCLUDES )
-    LIST( REMOVE_ITEM prl_files ${IPRL_EXCLUDES} )
+# - ??
+macro(_cet_install_perllib_without_list)
+  file(GLOB prl_files [^.]*.pm)
+  file(GLOB prl_files2 [^.]*.pm README)
+  if(IPRL_EXCLUDES)
+    list(REMOVE_ITEM prl_files ${IPRL_EXCLUDES})
   endif()
-  if( prl_files )
-    #message( STATUS "_cet_install_perllib_without_list: installing perl lib files ${prl_files} in ${perllib_install_dir}")
-    _cet_copy_perllib( LIST ${prl_files} )
-    _cet_perllib_config_setup( ${prl_files} )
-    INSTALL ( FILES ${prl_files2}
-              DESTINATION ${perllib_install_dir} )
-  endif( prl_files )
+
+  if(prl_files)
+    _cet_copy_perllib(LIST ${prl_files})
+    _cet_perllib_config_setup(${prl_files})
+    install(FILES ${prl_files2} DESTINATION ${perllib_install_dir})
+  endif()
+
   # now check subdirectories
-  if( IPRL_SUBDIRS )
-    foreach( sub ${IPRL_SUBDIRS} )
-      FILE(GLOB subdir_prl_files2
-                ${sub}/[^.]*.pm  
-		${sub}/README
-		)
-      FILE(GLOB subdir_prl_files ${sub}/[^.]*.pm )
-      #message( STATUS "found ${sub} files ${subdir_prl_files}")
-      if( IPRL_EXCLUDES )
-        LIST( REMOVE_ITEM subdir_prl_files ${IPRL_EXCLUDES} )
-        LIST( REMOVE_ITEM subdir_prl_files2 ${IPRL_EXCLUDES} )
-      endif()
-      if( subdir_prl_files )
-        _cet_copy_perllib( LIST ${subdir_prl_files} SUBDIR ${sub} )
-        _cet_perllib_config_setup( ${subdir_prl_files} )
-        INSTALL ( FILES ${subdir_prl_files2}
-                  DESTINATION ${perllib_install_dir}/${sub} )
-      endif( subdir_prl_files )
-    endforeach(sub)
-  endif( IPRL_SUBDIRS )
-endmacro( _cet_install_perllib_without_list )
+  if(IPRL_SUBDIRS)
+    foreach(sub ${IPRL_SUBDIRS})
+      file(GLOB subdir_prl_files2 ${sub}/[^.]*.pm ${sub}/README)
+      file(GLOB subdir_prl_files ${sub}/[^.]*.pm)
 
-macro( install_perllib   )
-  cmake_parse_arguments( IPRL "" "" "SUBDIRS;LIST;EXTRAS;EXCLUDES" ${ARGN})
-  _cet_current_subdir( TEST_SUBDIR )
-  STRING( REGEX REPLACE "^/${${product}_perllib_subdir}(.*)" "\\1" CURRENT_SUBDIR "${TEST_SUBDIR}" )
+      if(IPRL_EXCLUDES)
+        list(REMOVE_ITEM subdir_prl_files ${IPRL_EXCLUDES})
+        list(REMOVE_ITEM subdir_prl_files2 ${IPRL_EXCLUDES})
+      endif()
+      if(subdir_prl_files)
+        _cet_copy_perllib(LIST ${subdir_prl_files} SUBDIR ${sub})
+        _cet_perllib_config_setup(${subdir_prl_files})
+        install(FILES ${subdir_prl_files2} DESTINATION ${perllib_install_dir}/${sub})
+      endif()
+    endforeach()
+  endif()
+endmacro()
+
+# - ??
+macro(install_perllib)
+  cmake_parse_arguments(IPRL "" "" "SUBDIRS;LIST;EXTRAS;EXCLUDES" ${ARGN})
+  _cet_current_subdir(TEST_SUBDIR)
+  string(REGEX REPLACE "^/${${product}_perllib_subdir}(.*)" "\\1" CURRENT_SUBDIR "${TEST_SUBDIR}")
   set(perllib_install_dir "${${product}_perllib}${CURRENT_SUBDIR}")
   set(prlpathname "${${product}_perllib_subdir}${CURRENT_SUBDIR}")
-  #message( STATUS "install_perllib: perllib scripts will be installed in ${perllib_install_dir}" )
-  #message( STATUS "install_perllib: IPRL_SUBDIRS is ${IPRL_SUBDIRS}")
-  get_filename_component( CURRENT_SUBDIR_NAME "${CMAKE_CURRENT_SOURCE_DIR}" NAME )
-  #message( STATUS "install_perllib: CURRENT_SUBDIR_NAME is ${CURRENT_SUBDIR_NAME}" )
-  if( ${CURRENT_SUBDIR_NAME} MATCHES "CetSkelPlugins" )
+
+  get_filename_component(CURRENT_SUBDIR_NAME "${CMAKE_CURRENT_SOURCE_DIR}" NAME)
+
+  if(${CURRENT_SUBDIR_NAME} MATCHES "CetSkelPlugins")
     _cet_perl_plugin_version()
   endif()
 
-  if( IPRL_LIST )
-    if( IPRL_SUBDIRS )
-      message( FATAL_ERROR
-               "ERROR: call install_perllib with EITHER LIST or SUBDIRS but not both")
-    endif( IPRL_SUBDIRS )
-    _cet_copy_perllib( LIST ${IPRL_LIST} WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}")
-    _cet_perllib_config_setup( ${IPRL_LIST} )
-    INSTALL ( FILES  ${IPRL_LIST}
-              DESTINATION ${perllib_install_dir} )
+  if(IPRL_LIST)
+    if(IPRL_SUBDIRS)
+      message(FATAL_ERROR "ERROR: call install_perllib with EITHER LIST or SUBDIRS but not both")
+    endif()
+    _cet_copy_perllib(LIST ${IPRL_LIST} WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}")
+    _cet_perllib_config_setup(${IPRL_LIST})
+    install(FILES ${IPRL_LIST} DESTINATION ${perllib_install_dir})
   else()
-    if( IPRL_EXTRAS )
-      _cet_copy_perllib( LIST ${IPRL_EXTRAS} )
-      _cet_perllib_config_setup( ${IPRL_EXTRAS} )
-      INSTALL ( FILES  ${IPRL_EXTRAS}
-                DESTINATION ${perllib_install_dir} )
-    endif( IPRL_EXTRAS )
+    if(IPRL_EXTRAS)
+      _cet_copy_perllib(LIST ${IPRL_EXTRAS})
+      _cet_perllib_config_setup(${IPRL_EXTRAS})
+      install(FILES ${IPRL_EXTRAS} DESTINATION ${perllib_install_dir})
+    endif()
+
     _cet_install_perllib_without_list()
   endif()
-endmacro( install_perllib )
+endmacro()
+
