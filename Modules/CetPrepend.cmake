c2c69e344a2f613e4441f529220317bdd3d7c0c0
Lynn Garren <garren@fnal.gov>
2016-02-18 11:42:36 -0600

    use _cet_exclude_from_list

--- Modules/CetPrepend.cmake
+++ Modules/CetPrepend.cmake
@@ -1,14 +1,18 @@
 # internal function
+# not currently in use, but retained for future refernce
 
 function (_cet_prepend_directory OUTPUT_VAR prefix)
   set( fileList "" )
   message(STATUS "prepend ${prefix} to ${ARGN}")
   foreach( myfile ${ARGN} )
       get_filename_component( mydir "${myfile}" DIRECTORY )
-      if( NOT IS_ABSOLUTE "${mydir}" )
-      list(APPEND fileList "${prefix}/${myfile}")
+      if( IS_ABSOLUTE "${mydir}" )
+           list(APPEND fileList "${myfile}")
+      else()
+          list(APPEND fileList "${prefix}/${myfile}")
       endif()
   endforeach()
+  message(STATUS "_cet_prepend_directory new list ${fileList}" )
   set (${OUTPUT_VAR} "${fileList}" PARENT_SCOPE)
-  message(STATUS "_cet_prepend_directory new list ${OUTPUT_VAR}" )
+  message(STATUS "_cet_prepend_directory output ${OUTPUT_VAR}" )
 endfunction()
